c5b821eaf3524c006237c0ba86529277a0bad7de
HoselytaBravo <114197548+HoselytaBravo@users.noreply.github.com>
2022-09-22 18:53:14 -0600

    Add files via upload

---
+++ PROYECTO1/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace PROYECTO1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(162, 124);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(121, 228);
+            this.listBox2.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(29, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(115, 228);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 58);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GENERAR VALORES DE LA MATRIZ ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(446, 124);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(121, 228);
+            this.listBox3.TabIndex = 8;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 16;
+            this.listBox4.Location = new System.Drawing.Point(313, 124);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(115, 228);
+            this.listBox4.TabIndex = 7;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 16;
+            this.listBox5.Location = new System.Drawing.Point(784, 124);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(121, 228);
+            this.listBox5.TabIndex = 10;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 16;
+            this.listBox6.Location = new System.Drawing.Point(649, 124);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(115, 228);
+            this.listBox6.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(451, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 58);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "LIMPIAR ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 22);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "                    a:1, b:2, c:3";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(300, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(293, 22);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "                 Apple\", b: \"Microsoft\", c: \"Google\"";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(612, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(293, 22);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "               key1: true, key2: false, key3, undefined ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Violet;
+            this.label1.Font = new System.Drawing.Font("Forte", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(351, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 36);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "EJERCICIO 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(968, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PROYECTO1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROYECTO1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+            {
+
+        }
+
+        private void btnmatrices_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] valores = new string[9, 2];
+            //almacenar  valores
+            //{a:1, b:2, c:3}
+            valores[0, 0] = "a";
+            valores[0, 1] = "b";
+            valores[1, 0] = "c";
+            valores[1, 1] = "1";
+            valores[2, 0] = "2";
+            valores[2, 1] = "3";
+            //{ a: "Apple", b: "Microsoft", c: "Google" }
+            valores[3, 0] = "a";
+            valores[3, 1] = "b";
+            valores[4, 0] = "c";
+            valores[4, 1] = "Apple";
+            valores[5, 0] = "Google";
+            valores[5, 1] = "Microsoft";
+            //{ key1: true, key2: false, key3, undefined }
+            valores[6, 0] = "key1";
+            valores[6, 1] = "key2";
+            valores[7, 0] = "key3";
+            valores[7, 1] = "false";
+            valores[8, 0] = "true";
+            valores[8, 1] = "undefined";
+
+            //devolver valores en listbox
+            listBox1.Items.Add(valores[0, 0]);
+            listBox1.Items.Add(valores[0, 1]);
+            listBox1.Items.Add(valores[1, 0]);
+
+            listBox2.Items.Add(valores[1, 1]);
+            listBox2.Items.Add(valores[2, 0]);
+            listBox2.Items.Add(valores[2, 1]);
+
+            listBox3.Items.Add(valores[3, 0]);
+            listBox3.Items.Add(valores[3, 1]);
+            listBox3.Items.Add(valores[4, 0]);
+
+            listBox4.Items.Add(valores[4, 1]);
+            listBox4.Items.Add(valores[5, 0]);
+            listBox4.Items.Add(valores[5, 1]);
+
+            listBox5.Items.Add(valores[6, 0]);
+            listBox5.Items.Add(valores[6, 1]);
+            listBox5.Items.Add(valores[7, 0]);
+
+            listBox6.Items.Add(valores[7, 1]);
+            listBox6.Items.Add(valores[8, 0]);
+            listBox6.Items.Add(valores[8, 1]);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
