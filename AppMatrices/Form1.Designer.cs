c6a57c7d05ff36e6d4682ad7e424c892a8203a6f
HoselytaBravo <114197548+HoselytaBravo@users.noreply.github.com>
2022-09-22 18:27:06 -0600

    Add files via upload

---
+++ AppMatrices/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace AppMatrices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmatrices = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnmatrices
+            // 
+            this.btnmatrices.Location = new System.Drawing.Point(56, 38);
+            this.btnmatrices.Name = "btnmatrices";
+            this.btnmatrices.Size = new System.Drawing.Size(255, 58);
+            this.btnmatrices.TabIndex = 0;
+            this.btnmatrices.Text = "GENERAR VALORES DE LA MATRIZ ";
+            this.btnmatrices.UseVisualStyleBackColor = true;
+            this.btnmatrices.Click += new System.EventHandler(this.btnmatrices_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(29, 125);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 228);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(383, 125);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(308, 228);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GENERAR VALORES DE LA MATRIZ ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnmatrices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmatrices;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AppMatrices/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppMatrices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmatrices_Click(object sender, EventArgs e)
+        {
+            int[,] valores = new int[2, 2];
+            // almacenamos los valores en la matriz
+
+            valores[0, 0] = 100;
+            valores[0, 1] = 50;
+            valores[1, 0] = 60;
+            valores[1, 1] = 25;
+
+            // insertar los valores de la matriz en el listbox 
+            listBox1.Items.Add(valores[0, 0]);
+            listBox1.Items.Add(valores[0, 1]);
+            listBox1.Items.Add(valores[1, 0]);
+            listBox1.Items.Add(valores[1, 1]);
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[,] valores = new string[3, 3];
+            valores[0, 0] = "a";
+            valores[0, 1] = "1";
+            valores[1, 0] = "b";
+            valores[1, 1] = "2";
+            valores[2, 0] = "c";
+            valores[2, 1] = "3";
+
+            listBox2.Items.Add(valores[0, 0]);
+            listBox2.Items.Add(valores[0, 1]);
+            listBox2.Items.Add(valores[1, 0]);
+            listBox2.Items.Add(valores[1, 1]);
+            listBox2.Items.Add(valores[2, 0]);
+            listBox2.Items.Add(valores[2, 1]);
+
+        }
+    }
+}
